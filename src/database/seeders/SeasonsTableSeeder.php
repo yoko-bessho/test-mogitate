261a8e1097053c6d7ee00815c4039d3ebcc6e417
yoko bessho <yoko.murakami.11.18@gmail.com>
2025-05-27 22:22:59 +0900

    seasonのseedingまで

---
+++ src/database/seeders/SeasonsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class SeasonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $names = [
+            '春',
+            '夏',
+            '秋',
+            '冬',
+        ];
+
+        foreach ($names as $name) {
+            DB::table('seasons')->insert([
+                'name' => $name,
+            ]);
+        }
+    }
+}
