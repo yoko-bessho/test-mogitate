1c5e24c33cf63d535541b083ecafc7da9eaeefdf
yoko bessho <yoko.murakami.11.18@gmail.com>
2025-06-15 21:13:47 +0900

    storageへのアクセス調整

--- src/resources/views/products/index.blade.php
+++ src/resources/views/products/index.blade.php
@@ -57,7 +57,7 @@
             @foreach ($products as $product)
                 <a href="{{ route('products.edit', $product->id) }}" class="card-link">
                 <x-image-card
-                :src="asset('storage/' . $product->image)"
+                :src="asset($product->image)"
                 :alt="$product->name" :name="$product->name" :price="$product->price" />
                 </a>
             @endforeach
